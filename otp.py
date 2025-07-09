9b62a048a1ae3f89a5c026c4807f0b1bb0827b17
shivani <shivanich52@gmail.com>
2025-07-09 17:48:50 +0530

    otp file added

---
+++ otp.py
@@ -0,0 +1,12 @@
+import random
+
+otp = random.randint(100000, 999999)
+print("your OTP is:",otp)
+
+entered_otp = int(input("enter the otp:"))
+
+if entered_otp == otp:
+    print("OTP verified succesfully!")
+
+else:
+    print("invalid OTP. Please try again.")
